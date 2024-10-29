d9da82e3e1cbdf3deb25137aa0265a78c5c2e7e3
Mustafa Shah <shahmustafa051@gmail.com>
2024-10-29 08:30:04 +0100

    code can now run all the finctions

--- PriceDto.cs
+++ PriceDto.cs
@@ -9,10 +9,8 @@ namespace TaxaOpg
     public class PriceDto
     {
         public double pris { get; set; }
-        public double PrisPrKm { get; set; }
 
         public double StartPris = 0;
-        public string distance {  get; set; }
 
         public VehicleType CarType { get; set; }
         public SetTillaeg tillaeg { get; set; }
